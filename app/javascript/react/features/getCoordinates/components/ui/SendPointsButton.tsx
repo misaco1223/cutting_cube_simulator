afbfa4728a31c0be0290de68142b7a02280e1e41
玉水望朝 <tamamizumisa@tamamizunozomuasanoMacBook-Air.local>
2025-03-04 16:35:34 +0900

    修正)Reactからuuid付きで切断点を送信する、レスポンス受け取りまでの 間に「切断中」と表示する、fetchをasync/awaitの書き方に変更

--- app/javascript/react/features/getCoordinates/components/ui/SendPointsButton.tsx
+++ app/javascript/react/features/getCoordinates/components/ui/SendPointsButton.tsx
@@ -1,16 +1,57 @@
-import React, {useCallback} from "react";
+import React, {useCallback, useState} from "react";
 import * as THREE from "three";
 import { SendPointsButtonProps, faces } from "../../types/ThreeScene"
 import { isPointOnEdge } from "../../hooks/isPointOnEdge"
 
 const SendPointsButton = ({ points }: SendPointsButtonProps)=> {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const sendPointsToRails = useCallback(async() => {
+    setIsLoading(true);
+    const cutPoints = {
+      id: crypto.randomUUID(),
+      points: points.map((point: THREE.Vector3) => [
+      point.x,
+      point.y,
+      point.z
+    ])};
+
+    const csrfToken = document
+    .querySelector<HTMLMetaElement>('meta[name="csrf-token"]')
+    ?.getAttribute("content");
+
+    if (!csrfToken) {
+      console.error("CSRFトークンが取得できませんでした");
+      return;
+    }
+
+    try {
+      const response = await fetch("/cut_cube", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRF-Token": csrfToken,
+        },
+        body: JSON.stringify(cutPoints),
+      });
+
+      if (!response.ok) throw new Error("切断点の送信に失敗しました");
+
+      const data = await response.json();
+      console.log("切断点が送信されました:", data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("送信エラー:", error);
+      setIsLoading(false); 
+    }
+  }, [points]);
 
   if (points.length < 3 )
     return <div className="ml-4 font-bold">切断点を3つ選択してください</div>;
 
   if (points.length > 3 )
-   return <div className="text-red-500 font-bold ml-4"> 切断点は3つに絞ってください</div>;
-  
+    return <div className="text-red-500 font-bold ml-4">切断点は3つに絞ってください</div>;
+
   const isOnSameFace = faces.some((face) => {
     return points.every((point) =>
       face.some((edge) => {
@@ -22,53 +63,20 @@ const SendPointsButton = ({ points }: SendPointsButtonProps)=> {
 
   if (points.length === 3 && isOnSameFace)
     return <div className="text-red-500 font-bold ml-4">同じ面上の3点では切断できません</div>;
-    
-
-  const sendPointsToRails = useCallback(() => {
-
-    const cutPoints = points.map((point: THREE.Vector3) => [
-      point.x,
-      point.y,
-      point.z
-    ]);
-    console.log('送信する点は', points);
-
-    const csrfToken = document
-    .querySelector<HTMLMetaElement>('meta[name="csrf-token"]')
-    ?.getAttribute("content");
-
-    if (!csrfToken) {
-      console.error("CSRFトークンが取得できませんでした");
-      return;
-    }
-
-    fetch("/cut_points", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRF-Token": csrfToken,
-      },
-      body: JSON.stringify({ cutPoints }),
-    })
-    .then((response) => {
-      if (!response.ok) throw new Error("切断点の送信に失敗しました");
-      return response.json();
-    })
-    .then((data) => {
-      console.log("切断点が送信されました:", data);
-    })
-    .catch((error) => {
-      console.error("送信エラー:", error);
-    });
-  }, [points]);
 
   return (
-    <button 
-      onClick={sendPointsToRails}
-      className="button bg-green-700 bold text-white rounded mt-2 ml-4 py-2 px-4 hover:bg-green-700"
-    >
-      3点で切断する
-    </button>
+    <div>
+      {isLoading ? (
+        <p>切断中... しばらくお待ちください。</p>
+      ) : (
+        <button 
+          onClick={sendPointsToRails}
+          className="button bg-green-700 bold text-white rounded mt-2 ml-4 py-2 px-4 hover:bg-green-700"
+        >
+          3点で切断する
+        </button>
+      )}
+    </div>
   );
 }
 
